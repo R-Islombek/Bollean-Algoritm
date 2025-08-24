22451f7bf25692c652275bfcd2dc151f9925e63c
R-Islombek <islombekraximjanov97dev@gmail.com>
2025-08-24 23:58:32 +0500

    sa

--- js/main.js
+++ js/main.js
@@ -128,3 +128,5 @@ if ( ( a === b, a === c, b === c) && ( b === a   ) ) {
 // Uch xonali son berilgan. Jumlani rostlikka tekshiring: "Ushbu sonni chapdan o'qiganda ham, o'ngdan o'qiganda ham bir xil".
 
 // Shaxmat doskasining ikkita turli (x1, y1), (x2, y2) koordinalari berilgan (1-8 oraliqda yotuvchi butun sonlar). Jumlani rostlikka tekshiring: "Berilgan maydonlar bir xil rangda".
+
+//  Shaxmat doskasining ikkita turli (x1, y1), (x2, y2) koordinalari berilgan (1-8 oraliqda yotuvchi butun sonlar). Jumlani rostlikka tekshiring: "Ruh bir yurishda bir maydondan ikkinchisiga o'ta oladi".
