7c0728c3aa80dc8091f31fca5755543c578d5d70
R-Islombek <islombekraximjanov97dev@gmail.com>
2025-08-24 23:54:39 +0500

    s

--- js/main.js
+++ js/main.js
@@ -115,3 +115,6 @@ if ( ( a === b, a === c, b === c) && ( b === a   ) ) {
 // Uchta A, B, C butun sonlar berilgan. Jumlani rostlikka tekshiring: "A, B, C sonlaridan faqat bittasi musbat son".
 
 // . Uchta A, B, C butun sonlar berilgan. Jumlani rostlikka tekshiring: "A, B, C sonlardan faqat ikkitasi musbat son".
+
+// Musbat butun son berilgan. Jumlani rostlikka tekshiring: "Berilgan son ikki xonali juft son". 
+
