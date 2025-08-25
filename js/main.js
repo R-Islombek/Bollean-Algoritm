8cf6fffe4895369ea792c3c8f81192a5c466a3ac
R-Islombek <islombekraximjanov97dev@gmail.com>
2025-08-25 23:33:37 +0500

    sa

--- js/main.js
+++ js/main.js
@@ -182,4 +182,5 @@
 // Uchta A, B, C butun sonlar berilgan. Jumlani rostlikka tekshiring: "A, B, C sonlaridan faqat bittasi musbat son".
 
 // Uchta A, B, C butun sonlar berilgan. Jumlani rostlikka tekshiring: "A, B, C sonlardan faqat ikkitasi musbat son".
-Musbat butun son berilgan. Jumlani rostlikka tekshiring: "Berilgan son ikki xonali juft son".
+// Musbat butun son berilgan. Jumlani rostlikka tekshiring: "Berilgan son ikki xonali juft son".
+// Musbat butun son berilgan. Jumlani rostlikka tekshiring: "Berilgan son uch xonali toq". 
